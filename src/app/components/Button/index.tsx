cb9935b0657b5bd425b565d7d853ccd332e3218b
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-07 16:23:08 +0200

    added test for the Form shown as well as Adding Cards and removing cards

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -7,7 +7,11 @@ type MyButtonProps = {
 const MyButton = ({ onClick }: MyButtonProps) => {
   return (
     <div className="flex justify-center p-3 bg-blue-700 w-[150px] mx-auto">
-      <button onClick={onClick} className="text-white">
+      <button
+        data-testid="add-card-button"
+        onClick={onClick}
+        className="text-white"
+      >
         Add Cards
       </button>
     </div>
--- src/app/components/MainContent/MainContent.test.tsx
+++ src/app/components/MainContent/MainContent.test.tsx
@@ -35,7 +35,7 @@ describe("MainContent Component", () => {
     expect(cardsAfterRemoval.length).toBe(3);
   });
 
-  test("Check if a specific card is removed using queryBy", () => {
+  test("Check if a specific card is removed clicking X using queryBy", () => {
     render(<MainContent />);
     const project1 = screen.getByText("Project1");
     expect(project1).toBeInTheDocument();
@@ -44,5 +44,85 @@ describe("MainContent Component", () => {
 
     const project1AfterRemoval = screen.queryByText("Project1");
     expect(project1AfterRemoval).not.toBeInTheDocument();
+  });
+
+  test("Successfully shows the CardForm component when the Add Cards button is clicked", () => {
+    render(<MainContent />);
+    expect(screen.queryByPlaceholderText("Project Name")).toBeNull(); // Form not present yet
+
+    fireEvent.click(screen.getByText("Add Cards"));
+
+    expect(screen.getByPlaceholderText("Project Name")).toBeInTheDocument(); // Form is successfully shown
+  });
+
+  test("Successfully hides the CardForm component when Cancel button is clicked", () => {
+    render(<MainContent />);
+
+    fireEvent.click(screen.getByText("Add Cards"));
+    expect(screen.getByPlaceholderText("Project Name")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Project Link")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Technologies")).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText("Cancel"));
+
+    expect(screen.queryByPlaceholderText("Project Name")).toBeNull();
+    expect(screen.queryByPlaceholderText("Project Link")).toBeNull();
+    expect(screen.queryByPlaceholderText("Technologies")).toBeNull();
+  });
+
+  test("Successfully adds a new card after form is submitted", () => {
+    render(<MainContent />);
+
+    fireEvent.click(screen.getByTestId("add-card-button"));
+
+    fireEvent.change(screen.getByPlaceholderText("Project Name"), {
+      target: { value: "My New Project" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Project Link"), {
+      target: { value: "https://newproject.com" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Technologies"), {
+      target: { value: "HTML, CSS" },
+    });
+
+    fireEvent.click(screen.getByText("Add Card"));
+
+    expect(screen.getByText("My New Project")).toBeInTheDocument();
+  });
+
+  test("Successfully verifies the correct number of cards after adding a new one", () => {
+    render(<MainContent />);
+
+    const initialCardCount = screen.queryAllByRole("img").length;
+
+    fireEvent.click(screen.getByText("Add Cards"));
+    fireEvent.change(screen.getByPlaceholderText("Project Name"), {
+      target: { value: "Project B" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Project Link"), {
+      target: { value: "https://projectb.com" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Technologies"), {
+      target: { value: "TypeScript" },
+    });
+
+    fireEvent.click(screen.getByText("Add Card"));
+
+    const newCardCount = screen.queryAllByRole("img").length;
+    expect(newCardCount).toBe(initialCardCount + 1);
+
+    fireEvent.click(screen.getByText("Add Cards"));
+    fireEvent.change(screen.getByPlaceholderText("Project Name"), {
+      target: { value: "A New Project B" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Project Link"), {
+      target: { value: "https://aNEWprojectb.com" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Technologies"), {
+      target: { value: "TypeScript and NEXT JS" },
+    });
+    fireEvent.click(screen.getByText("Add Card"));
+    const reNewCardCount = screen.queryAllByRole("img").length;
+    expect(reNewCardCount).toBe(initialCardCount + 2);
   });
 });
