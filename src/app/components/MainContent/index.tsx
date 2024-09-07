a51c63deeda17fad2d000e717fec6d9a7847bb67
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-07 14:00:42 +0200

    added a mockup card test one

--- src/app/components/MainContent/index.tsx
+++ src/app/components/MainContent/index.tsx
@@ -1,7 +1,15 @@
 import MyButton from "../Button";
+import MyCard from "../Card";
 const MainContent = () => {
+  const MyRandomImage: string = "https://picsum.photos/200/300?random=1";
   return (
     <div>
+      <MyCard
+        project="Project1"
+        image={MyRandomImage}
+        link="https://www.widenisland.com"
+        tech="CSS, HTML, Wordpress, Javascript"
+      />
       <MyButton />
     </div>
   );
