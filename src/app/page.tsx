995f95b45bb39acc3790fc0745dfa9383583eaf6
Edwin Andermyr <andermyredwin@gmail.com>
2024-09-08 18:58:17 +0200

    adjusted the project as needed for Rob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import MyFooter from "./components/Footer";
 export default function Home() {
-  const navLinks = [
-    { name: "Home", href: "/" },
-    { name: "About", href: "/#" },
-    { name: "Contact Us", href: "/#" },
-  ];
-
-  const socialLinks = [
-    { name: "Instagram", href: "https://instagram.com" },
-    { name: "Github", href: "https://github.com" },
-    { name: "Twitter", href: "https://twitter.com" },
-  ];
-
-  return (
-    <main className="bg-gray-800">
-      <Header />
-      <MainContent />
-      <MyFooter navLinks={navLinks} socialLinks={socialLinks} />
-    </main>
-  );
+  return <main className="bg-gray-800">Good luck Boss!</main>;
 }
